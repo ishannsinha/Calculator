84683c98a9b759d22114d6081e0a4010822745b1
Ishan Sinha <ishansinha500@gmail.com>
2026-08-04 03:36:18 +0530

    Zero division error

--- calculator.py
+++ calculator.py
@@ -1,31 +1,27 @@
 print("====Python Calculator====")
 print("1.Addition \n2.Subtraction \n3.Multiplication \n4.Division \n5.Exit")
-a=int(input("Enter your Choice:"))
-while a!=5:
-    if a==1:
-        num1=float(input("Enter first number:"))
-        num2=float(input("Enter second number:"))
-        print(num1,"+",num2,"=",num1+num2)
-        break
-    elif a==2:
-        num1=float(input("Enter first number:"))
-        num2=float(input("Enter second number:"))
-        print(num1,"-",num2,"=",num1-num2)
-        break    
-    elif a==3:
-        num1=float(input("Enter first number:"))
-        num2=float(input("Enter second number:"))
-        print(num1,"*",num2,"=",num1*num2)
-        break
-    elif a==4:
-        num1=float(input("Enter first number:"))
-        num2=float(input("Enter second number:"))
-        if num2==0:
-            print("Error! Division by zero.")
-        else:
-            print(num1,"/",num2,"=",num1/num2)
-        break        
+a=int(input("Enter your Choice:")) 
+if a==1:
+    b=int(input("Enter first number:"))
+    c=int(input("Enter second number:"))
+    print(b,"+",c,"=",b+c)
+elif a==2:
+    b=int(input("Enter first number:"))
+    c=int(input("Enter second number:"))
+    print(b,"-",c,"=",b-c)
+elif a==3:
+    b=int(input("Enter first number:"))
+    c=int(input("Enter second number:"))
+    print(b,"*",c,"=",b*c)  
+elif a==4:
+    b=int(input("Enter first number:"))
+    c=int(input("Enter second number:"))
+    if c==0:
+        print("Error! Division by zero.")
     else:
-        print("Invalid Input")
-        break
-print("Exiting the calculator. Goodbye!")        
+        print(b,"/",c,"=",b/c)
+elif a==5:
+    print("Exiting the calculator. Goodbye!")   
+else:
+    print("Invalid input! Please enter a valid choice.")    
+print("Thank you for using the calculator.")                            

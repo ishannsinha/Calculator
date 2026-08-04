0ecc241eca95684cb2a978e48875ba37bb1aba4b
Ishan Sinha <ishansinha500@gmail.com>
2026-08-05 00:32:56 +0530

    Added loop and efficient syntax

--- calculator.py
+++ calculator.py
@@ -1,27 +1,23 @@
-print("====Python Calculator====")
-print("1.Addition \n2.Subtraction \n3.Multiplication \n4.Division \n5.Exit")
-a=int(input("Enter your Choice:")) 
-if a==1:
-    b=int(input("Enter first number:"))
-    c=int(input("Enter second number:"))
-    print(b,"+",c,"=",b+c)
-elif a==2:
-    b=int(input("Enter first number:"))
-    c=int(input("Enter second number:"))
-    print(b,"-",c,"=",b-c)
-elif a==3:
-    b=int(input("Enter first number:"))
-    c=int(input("Enter second number:"))
-    print(b,"*",c,"=",b*c)  
-elif a==4:
-    b=int(input("Enter first number:"))
-    c=int(input("Enter second number:"))
-    if c==0:
-        print("Error! Division by zero.")
+while(True):
+    print("====Python Calculator====")
+    print("1.Addition \n2.Subtraction \n3.Multiplication \n4.Division \n5.Exit")
+    a=int(input("Enter your Choice:")) 
+    if(1<=a<=4):
+        if(0<a<5):
+            num1=float(input("Enter first number:"))
+            num2=float(input("Enter second number:"))
+            if(a==1):
+                print(num1,"+",num2,"=",num1+num2)
+            elif(a==2):
+                print(num1,"-",num2,"=",num1-num2)
+            elif(a==3):
+                print(num1,"*",num2,"=",num1*num2)
+            elif(a==4):
+                if(num2!=0):
+                    print(num1,"/",num2,"=",num1/num2)
+                else:
+                    print("Error! Division by zero.")
     else:
-        print(b,"/",c,"=",b/c)
-elif a==5:
-    print("Exiting the calculator. Goodbye!")   
-else:
-    print("Invalid input! Please enter a valid choice.")    
-print("Thank you for using the calculator.")                            
+        print("Exiting the calculator. Goodbye!")
+        break
+print("Thank you for using the calculator. Goodbye!")
